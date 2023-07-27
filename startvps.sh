365808e7c575e745bea653d5dd2c0f0f8863a2ef
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-07-28 01:45:21 +0600

    Update startvps.sh

--- startvps.sh
+++ startvps.sh
@@ -1,72 +1,95 @@
 #!/bin/bash
 
+# Define color variables
 red='\033[1;31m'
 green='\033[1;32m'
 yellow='\033[1;33m'
 blue='\033[1;34m'
 light_cyan='\033[1;96m'
 reset='\033[0m'
-orange='\33[38;5;208m'
+orange='\033[38;5;208m'
 
-cd ~/ 2> /dev/null
+# Redirect error output to /dev/null and set important environment variables
+cd ~ || exit 1
 unset DBUS_LAUNCH
 export HOME="$(pwd)"
 export DISPLAY=":0"
-sudo kill -9 $(pgrep ngrok) &> /dev/null 2> /dev/null
-while [[ $server == "" ]] 2> /dev/null || [ -z $server ] 2> /dev/null; do
-    printf "${blue} Select your region:\n${yellow} 1. United States (Ohio)\n 2. Europe (Frankfurt)\n 3. Asia/Pacific (Singapore)\n 4. Australia (Sydney)\n 5. South America (Sao Paulo)\n 6. Japan (Tokyo)\n 7. India (Mumbai)\n 8. Exit\n\n${green}"
+
+# Kill any running ngrok and websockify processes
+sudo killall -q ngrok
+sudo killall -q websockify
+
+# Select the region for ngrok
+while [[ -z $server ]]; do
+    printf "${blue}Select your region:\n${yellow} 1. United States (Ohio)\n 2. Europe (Frankfurt)\n 3. Asia/Pacific (Singapore)\n 4. Australia (Sydney)\n 5. South America (Sao Paulo)\n 6. Japan (Tokyo)\n 7. India (Mumbai)\n 8. Exit\n\n${green}"
     read -p "Region: " server
-    if [[ $server -eq 1 ]] 2> /dev/null; then
-        regions="us"
-    else
-        if [[ $server -eq 2 ]] 2> /dev/null; then
-            regions="eu"
-        else
-            if [[ $server -eq 3 ]] 2> /dev/null; then
-                regions="ap"
-            else
-                if [[ $server -eq 4 ]] 2> /dev/null; then
-                    regions="au"
-                else
-                    if [[ $server -eq 5 ]] 2> /dev/null; then
-                        regions="sa"
-                    else
-                        if [[ $server -eq 6 ]] 2> /dev/null; then
-                            regions="jp"
-                        else
-                            if [[ $server -eq 7 ]] 2> /dev/null; then
-                                regions="in"
-                            else
-                                if [[ $server == "K" ]] 2> /dev/null || [[ $server == "k" ]] 2> /dev/null; then
-                                    exit 0
-                                else
-                                    unset server
-                                fi
-                            fi
-                        fi
-                    fi
-                fi
-            fi
-        fi
-    fi
+
+    case $server in
+        1) regions="us";;
+        2) regions="eu";;
+        3) regions="ap";;
+        4) regions="au";;
+        5) regions="sa";;
+        6) regions="jp";;
+        7) regions="in";;
+        [Kk]) exit 0;;
+        *) unset server;;
+    esac
 done
-nohup sudo ngrok tcp --region $regions 127.0.0.1:5900 &> /dev/null &
-vncserver -kill :0 &> /dev/null 2> /dev/null
+
+# Start ngrok and VNC server
+nohup sudo ngrok tcp --region "$regions" 127.0.0.1:5900 &> /dev/null &
+vncserver -kill :0 &> /dev/null
 sudo rm -rf /tmp/* 2> /dev/null
 vncserver :0
+
+# Start websockify for VNC access via web
+websockify -D --web=/usr/share/novnc/ --cert="$HOME/novnc.pem" 8080 localhost:5900
+
+# Configure TCP keepalive settings
 sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=10000 net.ipv4.tcp_keepalive_intvl=5000 net.ipv4.tcp_keepalive_probes=100
-sall="$(service  --status-all 2> /dev/null | grep '\-' | awk '{print $4}')"
+
+# Start all available services
+sall="$(service --status-all 2> /dev/null | grep '\-' | awk '{print $4}')"
 while IFS= read -r line; do
-    nohup sudo service "$line" start &> /dev/null 2> /dev/null &
+    nohup sudo service "$line" start &> /dev/null &
 done < <(printf '%s\n' "$sall")
+
 clear
-echo "You can use novnc server in browser to view your Desktop. 
-Just press web preview (on top right) and go to 8080 port and press the vnc.html link. 
-Or use the ip and put it to your VNC viewer"
-websockify -D --web=/usr/share/novnc/ --cert=$HOME/novnc.pem 8080 localhost:5900
+
+# Display information for accessing the VNC server
+echo "You can use novnc server in the browser to view your Desktop."
+echo "Just press web preview (on the top right) and go to port 8080 and press the vnc.html link."
+echo "Or use the IP and put it into your VNC viewer."
+
+# Get the public URL for the ngrok tunnel
 printf "\nYour IP Here: "
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+
+# Set the prompt with the default format
 export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 printf "\n\n"
-### This line by kmille36
-seq 1 9999999999999 | while read i; do echo -en "\r Running .     $i s /9999999999999 s";sleep 0.1;echo -en "\r Running ..    $i s /9999999999999 s";sleep 0.1;echo -en "\r Running ...   $i s /9999999999999 s";sleep 0.1;echo -en "\r Running ....  $i s /9999999999999 s";sleep 0.1;echo -en "\r Running ..... $i s /9999999999999 s";sleep 0.1;echo -en "\r Running     . $i s /9999999999999 s";sleep 0.1;echo -en "\r Running  .... $i s /9999999999999 s";sleep 0.1;echo -en "\r Running   ... $i s /9999999999999 s";sleep 0.1;echo -en "\r Running    .. $i s /9999999999999 s";sleep 0.1;echo -en "\r Running     . $i s /9999999999999 s";sleep 0.1; done
+
+# Delay loop for a specified time
+seq 1 9999999999999 | while read -r i; do
+    echo -en "\r Running .     $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running ..    $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running ...   $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running ....  $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running ..... $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running     . $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running  .... $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running   ... $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running    .. $i s /9999999999999 s"
+    sleep 0.1
+    echo -en "\r Running     . $i s /9999999999999 s"
+    sleep 0.1
+done
