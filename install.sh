3bc61c7ecdf97588a6b4833ce8e01afbbce9527e
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-07-28 00:17:32 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-#KhanhNguyen9872
-#Youtube: https://youtube.com/channel/UCG48mG78znU95DSxyCBffOg
-#Facebook: https://fb.me/khanh10a1
-#Phone: +84937927513
-echo "Preparing...."
-wget -O ngrok-stable-linux-amd64.zip "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/ngrok-stable-linux-amd64.zip?raw=true" > /dev/null 2>&1
+
+echo "Preparing to install...."
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1; rm ngrok-stable-linux-amd64.zip 2> /dev/null
-sudo mv ./ngrok /bin/ngrok; chmod 777 /bin/ngrok
+sudo mv ./ngrok /bin/ngrok; sudo chmod +x /bin/ngrok
 read -p "INSERT authtoken ngrok: " key
 ngrok authtoken $key
 echo ""
-echo "Installing Linux (Debian amd64)...."
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "Cloud Shell already runs on Debian. Just installing the DE and some apps(Xfce amd64)...."
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update -y > /dev/null 2>&1
-sudo apt install apt-transport-https ufw fish apache2 php sublime-text xfce4 xarchiver wine firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y
+sudo apt install code wget software-properties-common apt-transport-https ufw fish apache2 php xfce4 xarchiver firefox-esr mesa-utils xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y
+export installer="$(pwd)"
+cd ~ 2> /dev/null
 export HOME="$(pwd)"
 export DISPLAY=":0"
 cd $HOME 2> /dev/null
@@ -28,41 +27,21 @@ fi
 echo "set fish_greeting" > ~/.config/fish/config.fish
 chmod -R 777 ~/.config 2> /dev/null
 sudo printf '#!/bin/bash\ndbus-launch &> /dev/null\nautocutsel -fork\nxfce4-session\n' > ~/.vnc/xstartup
-wget -O startvps.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/startvps.sh" 2> /dev/null
-wget -O setupPS.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/setupPS.sh" 2> /dev/null
-wget -O apache2.conf "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/apache2.conf" 2> /dev/null
-wget -O vscode.deb "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/app/vscode_1.66.1_amd64.deb?raw=true" 2> /dev/null
+cd $installer 2> /dev/null
 sudo mv ./startvps.sh /bin/startvps 2> /dev/null
-sudo rm -rf ~/.bashrc 2> /dev/null
+sudo mv ~/.bashrc ~/.bashrc_old 2> /dev/null
+echo "Your ~/.bashrc is being modified. Backed up the the old .bashrc file as .bashrc_old"
 sudo mv ./setupPS.sh ~/.bashrc 2> /dev/null
-sudo rm -f /bin/wine 2> /dev/null
-sudo ln -s /etc/alternatives/wine64 /bin/wine 2> /dev/null
 sudo mv ./apache2.conf /etc/apache2/apache2.conf 2> /dev/null
-cat > "${HOME}/wine64_explorer.desktop" <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=wine64 explorer
-Comment=Run exe on Linux
-Exec=/bin/wine64 explorer.exe
-Icon=
-Path=
-Terminal=false
-StartupNotify=false
-EOL
-sudo chmod 777 ${HOME}/wine64_explorer.desktop
 sudo chmod 777 -R ~/.vnc 2> /dev/null
 sudo chmod 777 ~/.bashrc 2> /dev/null
 sudo chmod 777 /bin/startvps 2> /dev/null
-sudo chmod 777 /bin/wine 2> /dev/null
 sudo chmod 777 /etc/apache2/apache2.conf 2> /dev/null
-dpkg -i vscode.deb
-rm -rf ./vscode.deb 2> /dev/null
 sudo apt update -y > /dev/null 2>&1
 sudo apt autoremove -y
 if [ ! -d /usr/share/themes/Windows-10-Dark-master ] 2> /dev/null; then
   cd /usr/share/themes/ 2> /dev/null
-  wget -O Windows-10-Dark-master.zip "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/app/Windows-10-Dark-master.zip?raw=true" 2> /dev/null
+  sudo cp $installer/app/Windows-10-Dark-master.zip ./
   unzip -qq Windows-10-Dark-master.zip 2> /dev/null
   rm -f Windows-10-Dark-master.zip 2> /dev/null
 fi
