67b7cdda29276008f41c584e251c915dbbafe58b
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-07-28 03:04:52 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 echo "Preparing to install...."
 
 # Unzip and move ngrok binary
-unzip ngrok-stable-linux-amd64.zip > /dev/null
-rm ngrok-stable-linux-amd64.zip > /dev/null
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 sudo mv ./ngrok /bin/ngrok
 sudo chmod +x /bin/ngrok
 
@@ -15,17 +15,16 @@ ngrok authtoken "$key"
 
 # Inform about the following steps
 echo ""
-echo "Cloud Shell already runs on Debian. Just installing the DE and some apps (Xfce amd64)...."
+echo "Cloud Shell already runs on Debian. Just installing the DE (Xfce amd64) and some apps...."
 
 # Add Microsoft's GPG key and setup Visual Studio Code repository
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg > /dev/null 2>&1
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ > /dev/null 2>&1
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # Update package list and install necessary packages
-sudo apt update > /dev/null
-echo "Installing the required packages..."
-sudo apt install openssl code wget software-properties-common apt-transport-https ufw xfce4 xarchiver firefox-esr mesa-utils xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension novnc python3-websockify -y > /dev/null
+sudo apt update -y > /dev/null 2>&1
+sudo apt install fish code software-properties-common apt-transport-https ufw xfce4 xarchiver firefox-esr mesa-utils xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension novnc python3-websockify -y > /dev/null 2>&1
 
 # Set some environment variables
 export installer="$(pwd)"
@@ -33,30 +32,37 @@ cd ~/ || exit 1
 export HOME="$(pwd)"
 export DISPLAY=":0"
 cd "$HOME" || exit 1
-sudo mkdir ~/.vnc > /dev/null
+sudo mkdir ~/.vnc > /dev/null 2>&1
 
-# Set up some configuration
-if [ ! -d ~/.config ] ; then
-  sudo mkdir ~/.config > /dev/null
-chmod -R 777 ~/.config > /dev/null
+# Set up Fish shell configuration
+if [ ! -d ~/.config ] > /dev/null 2>&1; then
+  sudo mkdir ~/.config > /dev/null 2>&1
+fi
+if [ ! -d ~/.config/fish ] > /dev/null 2>&1; then
+  sudo mkdir ~/.config/fish > /dev/null 2>&1
+fi
+echo "set fish_greeting" > ~/.config/fish/config.fish
+chmod -R 777 ~/.config > /dev/null 2>&1
 sudo printf '#!/bin/bash\ndbus-launch &> /dev/null\nautocutsel -fork\nxfce4-session\n' > ~/.vnc/xstartup
 cd "$installer" || exit 1
-sudo mv ./startvps.sh /bin/startvps > /dev/null
-
+sudo mv ./startvps.sh /bin/startvps > /dev/null 2>&1
+sudo mv ~/.bashrc ~/.bashrc_old > /dev/null 2>&1
 
 # Inform about backup and update .bashrc
-sudo chmod 777 -R ~/.vnc > /dev/null
-sudo chmod 777 ~/.bashrc > /dev/null
-sudo chmod 777 /bin/startvps > /dev/null
-sudo apt update > /dev/null
-sudo apt autoremove -y > /dev/null
+echo "Your ~/.bashrc is being modified. Backed up the old .bashrc file as .bashrc_old"
+sudo mv ./setupPS.sh ~/.bashrc > /dev/null 2>&1
+sudo chmod 777 -R ~/.vnc > /dev/null 2>&1
+sudo chmod 777 ~/.bashrc > /dev/null 2>&1
+sudo chmod 777 /bin/startvps > /dev/null 2>&1
+sudo apt update -y > /dev/null 2>&1
+sudo apt autoremove -y > /dev/null 2>&1
 
 # Check and install Windows-10-Dark-master theme
-if [ ! -d /usr/share/themes/Windows-10-Dark-master ] ; then
+if [ ! -d /usr/share/themes/Windows-10-Dark-master ] > /dev/null 2>&1; then
   cd /usr/share/themes/ || exit 1
-  sudo cp "$installer"/app/Windows-10-Dark-master.zip ./ > /dev/null
-  unzip -qq Windows-10-Dark-master.zip > /dev/null
-  rm -f Windows-10-Dark-master.zip > /dev/null
+  sudo cp "$installer"/app/Windows-10-Dark-master.zip ./ > /dev/null 2>&1
+  unzip -qq Windows-10-Dark-master.zip > /dev/null 2>&1
+  rm -f Windows-10-Dark-master.zip > /dev/null 2>&1
 fi
 cd "$HOME" || exit 1
 clear
