25b0a78057e00afe38d184b4836302f1975a8c86
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-07-29 02:31:53 +0600

    Update startvps.sh

--- startvps.sh
+++ startvps.sh
@@ -67,7 +67,7 @@ sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=10000 net.ipv4.tcp_keepalive_in
 # Start all available services
 sall="$(service --status-all | grep '\-' | awk '{print $4}')"
 while IFS= read -r line; do
-    yes | nohup sudo service "$line" restart </dev/null &>/dev/null &
+    expect -c "spawn sudo service \"$line\" restart; expect \"*password*\"; send \"your_password_here\r\"; interact" &>/dev/null &
 done < <(printf '%s\n' "$sall")
 
 # Get the public URL for the ngrok tunnel
