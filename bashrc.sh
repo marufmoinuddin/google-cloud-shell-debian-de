34580c226b94fc69a4ab5d94a3ffb8d1a84342ba
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-08-19 20:16:42 +0600

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -122,5 +122,11 @@ de() {
   sudo bash install.sh
 }
 
+#Alias for "myip" command
+myip(){
+ wget -qO- https://ipecho.net/plain ; echo
+}
+
+
 source /google/devshell/bashrc.google
 export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
